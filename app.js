b2d03afb300c0bd9a837ac406cb49823981f27c1
Hien <trong.hien.ngo@gmail.com>
2022-05-25 12:03:05 +0200

    remplacement par methode fetch de l'appel de l'api meteo

--- app.js
+++ app.js
@@ -6,25 +6,19 @@ if ("geolocation" in navigator) {
       const key = "703bf068414b958e7ffeb989a4842ad2";
       const url = `https://api.openweathermap.org/data/2.5/weather?lon=${position.coords.longitude}&lat=${position.coords.latitude}&appid=${key}&units=metric`;
 
-      let req = new XMLHttpRequest();
-      req.open("GET", url);
-      req.responseType = "json";
-      req.send();
-
-      req.onload = function () {
-        if (req.readyState === XMLHttpRequest.DONE) {
-          if (req.status === 200) {
-            let res = req.response;
-            let temperature = res.main.temp;
-            let ville = res.name;
+      fetch(url).then(requete => {
+        if (requete.ok) {
+          requete.json().then(donnees => {
+            let temperature = donnees.main.temp;
+            let ville = donnees.name;
 
             document.querySelector("#temperature_label").textContent = temperature;
             document.querySelector("#ville").textContent = ville;
-          } else {
-            alert("Un problème est intervenu, merci de revenir plus tard.");
-          }
+          });
+        } else {
+          alert("Un problème est survenu");
         }
-      };
+      });
     },
     error,
     options,
@@ -50,27 +44,23 @@ function error() {
   recevoirTemperature(ville);
 }
 
-function recevoirTemperature(ville) {
+async function recevoirTemperature(ville) {
   const key = "703bf068414b958e7ffeb989a4842ad2";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${key}&units=metric`;
 
-  let req = new XMLHttpRequest();
-  req.open("GET", url);
-  req.responseType = "json";
-  req.send();
+  const requete = await fetch(url, {
+    method: "GET",
+  });
 
-  req.onload = function () {
-    if (req.readyState === XMLHttpRequest.DONE) {
-      if (req.status === 200) {
-        let res = req.response;
-        let temperature = res.main.temp;
-        let ville = res.name;
+  if (requete.ok) {
+    requete.json().then(donnees => {
+      let temperature = donnees.main.temp;
+      let ville = donnees.name;
 
-        document.querySelector("#temperature_label").textContent = temperature;
-        document.querySelector("#ville").textContent = ville;
-      } else {
-        alert("Un problème est intervenu, merci de revenir plus tard.");
-      }
-    }
-  };
+      document.querySelector("#temperature_label").textContent = temperature;
+      document.querySelector("#ville").textContent = ville;
+    });
+  } else {
+    alert("Un problème est survenu");
+  }
 }
